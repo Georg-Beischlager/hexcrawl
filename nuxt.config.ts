34dbb0a9feb45d229aea5af610b28e5d91867a49
Georg Beischlager <georg.beischlager@proton.me>
2025-05-04 16:56:30 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false,  },
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', 'vue3-pixi-nuxt']
+})
