0c3b77a572850e3a74b22494318bfe5f9b60345e
Georg Beischlager <georg.beischlager@proton.me>
2025-06-21 15:22:08 +0200

    - link underlining

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -7,18 +7,20 @@
       BACK
     </NuxtLink>
     <div class="lg:w-2/3 mt-4">
-        <h1 class="text-2xl mb-4">About</h1>
-        <p class="mb-4">
-            Das ist der zentrale HUB für die <a href="https://openlegendrpg.com/">Openlegends RPG</a> Kampagne "Black Moon".<br>
-            H.A.N.N.A.H ist die Bordintelligenz der Basis, diese Webseite bietet ein Interface um direkt mit ihren Daten zu interagieren.
-        </p>
-        <p>
-            Die Kampagne wird im <a href="https://www.youtube.com/watch?v=oGAC-gBoX9k">"West Marches"</a> Stil geführt, das heißt es gibt mehrere DMs.
-            Das erfordert einen erhöhten Grad an Koordination, diese Webseite soll das unterstützen.
-        </p>
-        <p>
-            Kontakt: georg.beischlager _at_ proton.me
-        </p>
+      <h1 class="text-2xl mb-4">
+        About
+      </h1>
+      <p class="mb-4">
+        Das ist der zentrale HUB für die <a href="https://openlegendrpg.com/" class="underline">Openlegends RPG</a> Kampagne "Black Moon".<br>
+        H.A.N.N.A.H ist die Bordintelligenz der Basis, diese Webseite bietet ein Interface um direkt mit ihren Daten zu interagieren.
+      </p>
+      <p>
+        Die Kampagne wird im <a href="https://www.youtube.com/watch?v=oGAC-gBoX9k" class="underline">"West Marches"</a> Stil geführt, das heißt es gibt mehrere DMs.
+        Das erfordert einen erhöhten Grad an Koordination, diese Webseite soll das unterstützen.
+      </p>
+      <p>
+        Kontakt: georg.beischlager _at_ proton.me
+      </p>
     </div>
   </div>
 </template>
